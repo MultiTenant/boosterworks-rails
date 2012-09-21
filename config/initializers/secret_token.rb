0745570b5bc7bf2d22100d4ff10395fa7bb5a913
Seth Hochberg <shochberg@sbxind.com>
2012-09-20 23:32:36 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BoosterworksRails::Application.config.secret_token = '433a8c1b9f8398b4ed293b25ccbf8d37f7bc464f1e4f94913a1c72b832b637fb202a31f6ec01fed959a416a052c489418325ab044651bb8366c9e442f33357a8'
